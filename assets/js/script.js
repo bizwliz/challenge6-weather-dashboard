bf4a23f9ed8eecd67ef6c4dba9be995ed4faf5b1
bizwliz <glissette1999@gmail.com>
2023-07-27 18:25:34 -0400

    input id's and base of code, started some js

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+var apiKey="59058ebd7030510abec5b6feff5ed764"
+var titleEl=document.getElementById("title")
+var tempEl=document.getElementById("temp")
+var windEl=document.getElementById("wind")
+var humidityEl=document.getElementById("humidity")
+
