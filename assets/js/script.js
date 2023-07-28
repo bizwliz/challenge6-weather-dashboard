b31f323bdd7f2eae32d1ec5a8958ae736e0e6273
bizwliz <glissette1999@gmail.com>
2023-07-27 22:56:39 -0400

    added apis and functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,41 @@ var titleEl=document.getElementById("title")
 var tempEl=document.getElementById("temp")
 var windEl=document.getElementById("wind")
 var humidityEl=document.getElementById("humidity")
+var searchBtn=document.getElementById("search-btn")
+var cityInput=document.getElementById("city-input")
+
+function searchCity(){
+    var cityName=cityInput.value
+
+    displayWeather(cityName)
+}
+
+function displayWeather(cityName){
+    var url="https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid="+apiKey+"&units=imperial"
+
+    fetch(url)
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(currentData){
+        console.log(currentData)
+    })
+
+    var forecastUrl="https://api.openweathermap.org/data/2.5/forecast?q="+cityName+"&appid="+apiKey+"&units=imperial"
+
+    fetch(forecastUrl)
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(forecastData){
+        console.log(forecastData)
+        var forecastArr=forecastData.list
+       for (let i = 3; i < forecastArr.length; i=i+8){
+        console.log(forecastArr[i])
+       }
+    })
+}
+
+
+searchBtn.addEventListener("click", searchCity)
 
