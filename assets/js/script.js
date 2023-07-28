a3e19d1420f6a11cd366bc0d804ce1c16819f6bc
bizwliz <glissette1999@gmail.com>
2023-07-28 00:23:29 -0400

    fixed title, tried fixing bug. will continue

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,36 +13,49 @@ function searchCity(){
     displayWeather(cityName)
 }
 
-function displayWeather(cityName){
-    var url="https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid="+apiKey+"&units=imperial"
-
+function displayWeather(cityName) {
+    var url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey + "&units=imperial";
+  
     fetch(url)
-    .then(function(response){
-        return response.json()
-    })
-    .then(function(currentData){
-        console.log(currentData)
-        titleEl.innerHTML=currentData.name+dayjs.unix(currentData.dt).format("(MM/DD/YYYY)")+"<img src='https://openweathermap.org/img/wn/"+currentData.weather[0].icon+"@2x.png'>"
-    })
-
-    var forecastUrl="https://api.openweathermap.org/data/2.5/forecast?q="+cityName+"&appid="+apiKey+"&units=imperial"
-
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (currentData) {
+        console.log(currentData);
+        titleEl.innerHTML = currentData.name + dayjs.unix(currentData.dt).format("(MM/DD/YYYY)") + "<img src='https://openweathermap.org/img/wn/" + currentData.weather[0].icon + "@2x.png'>";
+      });
+  
+    var forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey + "&units=imperial";
+  
     fetch(forecastUrl)
-    .then(function(response){
-        return response.json()
-    })
-    .then(function(forecastData){
-        console.log(forecastData)
-        var forecastArr=forecastData.list
-        fivedayforecastEl.textContent=""
-       for (let i = 3; i < forecastArr.length; i=i+8){
-        console.log(forecastArr[i])
-        var cardTitle=document.getElementById("card-title"+j)
-        cardTitle.textContent=dayjs.unix(forecastArr[i].dt).format("(MM/DD/YYYY)")
-       }
-    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (forecastData) {
+        console.log(forecastData);
+        var forecastArr = forecastData.list;
+  
+        // Clear the forecast section here, before updating it with the new data
+        fivedayforecastEl.textContent = "";
+  
+        var j = 0;
+        for (let i = 3; i < forecastArr.length; i = i + 8) {
+          console.log(forecastArr[i]);
+          var cardTitle = document.getElementById("card-title" + j);
+            if (cardTitle) {
+            cardTitle.textContent = dayjs.unix(forecastArr[i].dt).format("(MM/DD/YYYY)");
 }
 
+        var temp = document.getElementById("temp" + j);
+            if (temp) {
+            temp.textContent = forecastArr[i].main.temp;
+}
+}
+});
+}
 
-searchBtn.addEventListener("click", searchCity)
-
+document.addEventListener("DOMContentLoaded", function() {
+    var searchBtn = document.getElementById("search-btn");
+    searchBtn.addEventListener("click", searchCity);
+  });
+  
